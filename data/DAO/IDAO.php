530364fde57131a63ce8a1e2bff33ed4b86af7ae
DUVOISIN Nicolas <d16005292@a-maq-iutinfo.salsa.univ-amu.fr>
2018-11-09 13:02:17 +0100

    c'est parti

---
+++ data/DAO/IDAO.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d16005292
+ * Date: 09/11/18
+ * Time: 13:00
+ */
+
+interface IDAO {
+
+}
---
+++ model/UniqueConnection.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: d16005292
+ * Date: 09/11/18
+ * Time: 13:01
+ */
+
+class UniqueConnection
+{
+
+}
