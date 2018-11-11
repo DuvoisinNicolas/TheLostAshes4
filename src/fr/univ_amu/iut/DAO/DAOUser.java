93d1898b3e66045218297e2df9af0f4623d82475
DuvoisinNicolas <nicolasd130@gmail.com>
2018-11-11 23:08:41 +0100

    lol

--- src/fr/univ_amu/iut/DAO/DAOUser.java
+++ src/fr/univ_amu/iut/DAO/DAOUser.java
@@ -1,4 +1,23 @@
 package fr.univ_amu.iut.DAO;
 
+import fr.univ_amu.iut.Mappers.UserMapper;
+import fr.univ_amu.iut.UniqueConnection;
+import fr.univ_amu.iut.beans.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class DAOUser {
+
+    private Connection connection;
+
+    public DAOUser() throws SQLException {
+        connection = UniqueConnection.getInstance().getConnection();
+
+    }
+
+    public List<User> findAll() {
+
+    }
 }
--- src/fr/univ_amu/iut/Mappers/UserMapper.java
+++ src/fr/univ_amu/iut/Mappers/UserMapper.java
@@ -8,13 +8,6 @@ import java.sql.SQLException;
 public class UserMapper {
     public User mapRow(ResultSet resultSet) throws SQLException {
         User etudiant = new User();
-        etudiant.setNumEt(resultSet.getInt("NUM_ET"));
-        etudiant.setNomEt(resultSet.getString("NOM_ET"));
-        etudiant.setPrenomEt(resultSet.getString("PRENOM_ET"));
-        etudiant.setCpEt(resultSet.getString("CP_ET"));
-        etudiant.setVilleEt(resultSet.getString("VILLE_ET"));
-        etudiant.setAnnee(resultSet.getInt("ANNEE"));
-        etudiant.setGroupe(resultSet.getInt("GROUPE"));
         return etudiant;
     }
 }
