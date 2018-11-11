01cd9e03b79780e60b0aa7dd721c8552510e4d42
DuvoisinNicolas <nicolasd130@gmail.com>
2018-11-11 17:53:11 +0100

    suppression du travail

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/library/formatPage.php';
-
-startPage();
-?>
-
-
-<?php endPage(); ?>
--- library/mapLibrary.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: d16005292
- * Date: 09/11/18
- * Time: 12:59
- */
