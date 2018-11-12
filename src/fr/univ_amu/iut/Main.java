336af8df44007ef3bf25e49b1ac6de11d06bf4e4
DUVOISIN Nicolas <d16005292@a-maq-iutinfo.salsa.univ-amu.fr>
2018-11-12 15:05:49 +0100

    ajout du début de l'interface

--- src/fr/univ_amu/iut/Main.java
+++ src/fr/univ_amu/iut/Main.java
@@ -1,13 +1,33 @@
 package fr.univ_amu.iut;
 
-import fr.univ_amu.iut.DAO.DAOUser;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+    private BorderPane root = new BorderPane();
+    /**
+     * Hauteur et largeur de la fenètre
+     */
+    private int height = 800;
+    private int width = 600;
+
+    /**
+     * Taille et police des titres et des textes
+     */
+    private Font fontTitle = new Font("Arial",20);
+    private Font fontText = new Font("Arial",40);
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -15,9 +35,48 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("The Lost Ashes 4");
-        StackPane root = new StackPane();
-        Scene scene = new Scene(root,800,600);
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(root,width,height);
         primaryStage.setScene(scene);
+        connectionInterface();
         primaryStage.show();
+    }
+
+    /**
+     * Interface de connexion
+     */
+    private void connectionInterface () {
+        root.getChildren().clear();
+
+        // Titre en haut de page
+        HBox top = new HBox();
+        Label title = new Label("The Lost Ashes 4");
+        title.setFont(fontTitle);
+        top.setAlignment(Pos.CENTER);
+        top.getChildren().add(title);
+        root.setTop(top);
+
+        // Grille centrale
+        GridPane center = new GridPane();
+
+        // Partie de gauche -> Connexion
+        VBox login = new VBox();
+        login.setAlignment(Pos.CENTER);
+        Label loginText = new Label("Connexion");
+        TextField loginField = new TextField();
+        loginField.setPromptText("Identifiant");
+        PasswordField pwdField = new PasswordField();
+        pwdField.setPromptText("Mot de Passe");
+
+        login.getChildren().addAll(loginText,loginField,pwdField);
+
+        // Partie de droite -> Inscription
+        VBox register = new VBox();
+
+        center.add(login,0,0);
+
+        root.setCenter(center);
+
+
     }
 }
